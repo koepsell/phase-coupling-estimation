096221c2b6db3208b3d513ae1fa24737335260ce
Charles Cadieu <c.cadieu@gmail.com>
2008-06-12 16:10:50 -0700

    started matlab distribution

---
+++ matlab/fit_model.m
@@ -0,0 +1,13 @@
+function K = fit_model(p,vars)
+
+% prepare phaseinput, and parameters
+[d,nz] = size(p)
+
+z = [ exp(1j*p); exp(-1j*p)];
+
+% call fill_matrix
+
+% solve linear system of equations
+
+% prepare result for return
+
---
+++ matlab/setup.m
@@ -0,0 +1,21 @@
+% AUTORIGHTS
+
+mex fill_matrix
+
+% test
+
+load test_data data M_true
+
+K_fit = fit_model(data);
+
+hval = max(max(K(:),max(K_true(:))));
+
+figure(1)
+subplot(121)
+imagesc(K_true,[-1 1]*hval)
+subplot(122)
+imagesc(K_fit,[-1 1]*hval)
+
+K_error = mean(abs(K_true(:)-K_fit(:)));
+
+fprintf('\n mean-absolute-difference=%6.8f ; expect: xxxx',K_error);
